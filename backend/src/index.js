6fff9f007d933ee202d56c4acb22a5d47a9a1a7a
rangona barooah <rangonabarooah@rangonas-MacBook-Air.local>
2025-12-29 18:45:28 +0530

    test3

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,13 +6,13 @@ import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
-import { app,server } from './lib/socket.js';
+import { app, server } from './lib/socket.js';
 
 import path from 'path'; 
 
 dotenv.config();
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 
 const __dirname = path.resolve(); 
 
@@ -20,7 +20,7 @@ app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", // Adjust to your frontend URL
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -30,7 +30,10 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html')));
+  
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+  });
 }
 
 server.listen(PORT, () => {
