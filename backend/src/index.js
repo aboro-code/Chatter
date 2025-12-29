5ff9563fe793b3365a5ab233e64876bdc5f6b787
rangona barooah <rangonabarooah@rangonas-MacBook-Air.local>
2025-12-29 17:41:44 +0530

    deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,16 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
 import { app,server } from './lib/socket.js';
 
+import path from 'path'; 
+
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT ;
 
-app.use(express.json());
+const __dirname = path.resolve(); 
+
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173", // Adjust to your frontend URL
@@ -22,6 +27,11 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html')));
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`); 
